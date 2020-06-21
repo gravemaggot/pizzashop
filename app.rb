ccef52378ebd66e359964fa290981b13939e5376
Valdemar <gravemaggot1@gmail.com>
2020-06-21 16:50:38 +0300

    latest

--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
+require 'base64'
 
 set :database, 'sqlite3:pizzashop.db'
 
@@ -11,9 +12,11 @@ class Product < ActiveRecord::Base
 end
 
 get '/' do
+  @products = Product.all
   erb :index
 end
 
 get '/about' do
+  @encoded_file = Base64.encode64(File.open("./public/media/examples/01. The World Is Yours.mp3", "rb").read)
   erb :about
 end
