2acb983f7a745c161434e8097c60dc5d59b24f80
Valdemar <gravemaggot1@gmail.com>
2020-06-22 19:09:34 +0300

    some thinks

--- public/app.js
+++ public/app.js
@@ -16,8 +16,23 @@ function something()
 function add_to_cart(id)
 {
     var key = 'product_' + id;
-    
     var qty = window.localStorage.getItem(key);
+
     qty = qty * 1 + 1;
+
     window.localStorage.setItem(key + id, qty);
+}
+
+function cart_get_number_of_items()
+{
+    var cnt = 0;
+
+    for(var i = 0; i < window.localStorage.length; i++){
+        key = window.localStorage.key(i);
+        val = window.localStorage.getItem(key);
+
+        if (key.indexOf('product_') >= 0){
+            cnt = cnt + Number(val);
+        }
+    }
 }
